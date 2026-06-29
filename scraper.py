88edc6311440e8acc3ca2ad4586ab523717c270b
gomesrussel-commits <gomes.russel@gmail.com>
2026-06-30 05:01:27 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,3 +8,8 @@ BASE_URL = "https://commercial.bkt-tires.com"
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
 }
+import json
+import re
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
