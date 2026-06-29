3a339554dfe3ee87262ec28db7ce839b73cddd11
gomesrussel-commits <gomes.russel@gmail.com>
2026-06-30 04:47:12 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,16 @@
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import re
 
+BASE_URL = "https://commercial.bkt-tires.com"
+START_URL = "https://commercial.bkt-tires.com/en-in/products-search/"
+
+all_products = []
+
+
+def clean(text):
+    if not text:
+        return ""
+    return re.sub(r"\s+", " ", text).strip()
